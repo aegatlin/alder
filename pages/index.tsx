3356a8c7be2642dcf9ca17cae8359dafa5e72ffc
Austin Gatlin <austin@gatlin.io>
2023-01-13 11:53:25 +1030

    Style fix.

--- pages/index.tsx
+++ pages/index.tsx
@@ -36,7 +36,7 @@ export default function Index() {
   return (
     <main className="flex flex-col items-center">
       <h1 className="text-6xl">App</h1>
-      <div className=" space-y-4 p-4">
+      <div className="space-y-4 p-4">
         {list.map((item) => (
           <Item key={item.id} item={item} removeItem={removeItem} />
         ))}
@@ -50,7 +50,7 @@ export default function Index() {
 function Item({ item, removeItem }) {
   return (
     <div className="items-center rounded-2xl border p-4">
-      <div className="flex">
+      <div className="flex justify-between">
         <div className="mr-4 flex items-center">{item.value}</div>
         <Button onClick={() => removeItem(item.id)}>X</Button>
       </div>
