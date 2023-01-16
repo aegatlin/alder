eecb7faaa2f3f1b63ff2e80a117e36be408e06f5
Austin Gatlin <austin@gatlin.io>
2023-01-16 17:25:01 +1030

    Fix type error.

--- lib/core/InputText.tsx
+++ lib/core/InputText.tsx
@@ -19,7 +19,7 @@ export function InputText({
   onBlur,
   autoFocus,
 }: InputProps) {
-  const ref = useRef(null)
+  const ref = useRef<HTMLInputElement>(null)
 
   useEffect(() => {
     if (autoFocus) {
