7d8df950a37b55084d3971f4fcbb415d3719e687
Austin Gatlin <austin@gatlin.io>
2023-01-16 17:52:59 +1030

    Add simple manifest.json for PWA support.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,13 @@
+import Head from 'next/head'
 import Script from 'next/script'
 import '../lib/styles.css'
 
 function App({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <link rel="manifest" href="/manifest.json" />
+      </Head>
       <Component {...pageProps} />
       <Script src="/localforage.js" />
     </>
